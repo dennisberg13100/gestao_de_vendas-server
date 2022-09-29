29263d6f895625c147e2acc917c548ef25ebd6d0
Dennis van den Berg <dennisvandenberg13100@gmail.com>
2022-09-29 14:35:47 -0300

    date test

--- models/Client.js
+++ models/Client.js
@@ -3,17 +3,12 @@ const mongoose = require('mongoose')
 const ClientSchema = new mongoose.Schema({
     clientName: {
         type: String, 
-        require: true, 
-        min: 3, 
-        max: 50, 
     },
     cpf: {
         type: Number,
-        required: true, 
     }, 
     phoneNumber: {
         type: Number, 
-        required: true,
     },
     gender: {
         type: String, 
